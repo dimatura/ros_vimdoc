abd29947a8692cca3940789eefd0e78066d1cb32
Daniel Maturana <dimatura@cmu.edu>
2013-10-10 02:27:40 -0400

    bla

---
+++ roswiki_raw/example_wget_docbook.sh
@@ -0,0 +1,1 @@
+wget "http://wiki.ros.org/roslaunch/Commandline Tools?action=RenderAsDocbook" --user-agent="Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1" -O roslaunch_Commandline.docbook
